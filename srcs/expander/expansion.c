691a99acae26774c467927bb0eeac3a45bb88ad5
Garance CLAUSSE <gclausse@e1r6p16.42.fr>
2022-04-27 10:34:44 +0200

    without leaks

--- srcs/expander/expansion.c
+++ srcs/expander/expansion.c
@@ -31,17 +31,23 @@ char	*search_var(t_var_list *dst, char *var_name, int j)
 	return ("");
 }
 
+
 int expand_var (t_token *token, t_env *env)
 {
 	char	*str_expand;
+	char	*str_base;
+	int		quote;
 	int		i;
 	int		j;
 
 	i = 0;
+	quote = 0;
 	while (token->content[i])
 	{
 	//	printf("token->content[i] == %c, i == %d \n", token->content[i], i);
-		if (token->content[i] == '$')
+		if (token->content[i] == '\'')
+			quote++;
+		if (token->content[i] == '$' && (quote % 2 == 0))
 		{
 			j = 1;
 			while (ft_isalnum(token->content[j + i]) == 1)
@@ -49,8 +55,13 @@ int expand_var (t_token *token, t_env *env)
 			str_expand = search_var(&env->env_var, &token->content[i + 1], j);
 			if (str_expand == NULL)
 				str_expand = search_var(&env->shell_var, &token->content[i + 1], j);
+			str_base = ft_substr(token->content, 0, i);
 	//		printf("ft_substr(token->content, 0, i) == %s, str_expand == %s, &token->content[j + i] = %s\n", ft_substr(token->content, 0, i), str_expand, &token->content[j + i]);
-			token->content = ft_strjoin3(ft_substr(token->content, 0, i), str_expand, token->content + (j + i));
+			token->content = ft_strjoin3(str_base, str_expand, token->content + (j + i));
+			if (str_base)
+				free(str_base);
+			if (str_expand)
+				free(str_expand);
 	//		printf("token->content = %s\n", token->content);
 			i = 0;
 		}
