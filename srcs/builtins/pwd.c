ac1ec5750f7a29058a1039f6f6443a49047d4c25
Garance CLAUSSE <gclausse@e3r7p20.clusters.42paris.fr>
2022-04-28 12:32:56 +0200

    pwd with getcwd functiun and error

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:08:53 by gclausse          #+#    #+#             */
-/*   Updated: 2022/04/28 12:31:27 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/04/28 12:32:42 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	pwd(char **args)
 	cur_dir = getcwd(buf, 4096);
 	if (cur_dir == NULL)
 	{
-		write (1, "pwd: too many arguments\n", 25);
+		write (1, "pwd: path not found\n", 21);
 		return (1);
 	}
 	write (1, cur_dir, ft_strlen(cur_dir));
