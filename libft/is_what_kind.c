cd8a7ab61b74a7ddbc4db74a4a48a0eb0380d288
Garance CLAUSSE <gclausse@e1r9p17.42.fr>
2022-05-11 20:48:54 +0200

    Expand doesn't work anymore...

--- libft/is_what_kind.c
+++ libft/is_what_kind.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:15:39 by gclausse          #+#    #+#             */
-/*   Updated: 2022/05/11 16:05:25 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:40:01 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,24 +46,29 @@ int	ft_isunderscore(int c)
 int	is_valid_identifier(char *str, char *arg)
 {
 	int		i;
+	int		pbm;
 	char	*msg;
 
 	i = 0;
-	while (arg[i] && arg[i] != '=')
+	pbm = 0;
+	while (arg[i])
 	{
-		if ((ft_isalnum(arg[i]) && ft_isdigit(arg[i])
-				&& ft_isunderscore(arg[i]))
-				|| ft_isdigit(arg[0]) == 0)
-		{
-			msg = ft_strjoin3(str, ": '", arg);
-			if (!msg)
-				return (1);
-			ft_putstr_fd(msg, 2);
-			ft_putstr_fd("' : not a valid identifier\n", 2);
-			free(msg);
-			return (1);
-		}
+		if (arg[i] == '=' && ft_strcmp("assign", str) == 0)
+			break ;
+		if ((ft_isalnum(arg[i]) && ft_isunderscore(arg[i]) && arg[i] != '=' && arg[i] != '$')
+		|| ft_isdigit(arg[0]) == 0 || arg[0] == '=')
+					pbm++;	
 		i++;
+	}
+	if (pbm != 0 || !ft_strcmp(arg, ""))
+	{
+		msg = ft_strjoin3(str, ": '", arg);
+		if (!msg)
+			return (1);
+		ft_putstr_fd(msg, 2);
+		ft_putstr_fd("' : not a valid identifier\n", 2);
+		free(msg);
+		return (1);
 	}
 	return (0);
 }
--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,23 +6,18 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:08:53 by gclausse          #+#    #+#             */
-/*   Updated: 2022/05/11 14:18:51 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:36:25 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	pwd(char **args)
+int	pwd(void)
 {
 	char	*cur_dir;
 	char	*buf;
 
 	buf = NULL;
-	if (*args)
-	{
-		ft_putstr_fd("pwd: too many arguments\n", 2);
-		return (1);
-	}
 	cur_dir = getcwd(buf, 4096);
 	if (cur_dir == NULL)
 	{
