184d856be1548c5170a5a42ef9ee9997e22d5e17
Garance CLAUSSE <gclausse@e2r7p21.clusters.42paris.fr>
2022-05-12 12:29:41 +0200

    Expansion pbm agaaaaaaaaaaaaaaaaaaaaaaaaain

--- libft/is_what_kind.c
+++ libft/is_what_kind.c
@@ -6,7 +6,7 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 10:15:39 by gclausse          #+#    #+#             */
-/*   Updated: 2022/05/12 11:33:25 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:28:07 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	is_valid_identifier(char *str, char *arg)
 	pbm = 0;
 	while (arg[i])
 	{
-		if (arg[i] == '=')
+		if (i != 0 && arg[i] == '=')
 			break ;
 		if ((ft_isalnum(arg[i]) && ft_isunderscore(arg[i])
 			&& arg[i] != '=')
